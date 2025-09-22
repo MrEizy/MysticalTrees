13fa455b0eaef173162af34f62e7d2317e10d934
MrEizy <MrEizy@hotmail.com>
2025-09-22 09:43:45 +0200

    start on jei integration

---
+++ src/main/java/net/mreizy/mysticaltrees/compat/JEIModPlugin.java
@@ -0,0 +1,40 @@
+package net.mreizy.mysticaltrees.compat;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.mreizy.mysticaltrees.MysticalTrees;
+import net.mreizy.mysticaltrees.recipe.ModRecipes;
+import net.mreizy.mysticaltrees.recipe.PedestalRecipe;
+
+import java.util.List;
+
+@JeiPlugin
+public class JEIModPlugin implements IModPlugin {
+
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ResourceLocation.fromNamespaceAndPath(MysticalTrees.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new PedestalRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
+
+        List<PedestalRecipe> pedestalRecipes = recipeManager
+                .getAllRecipesFor(ModRecipes.PEDESTAL_TYPE.get()).stream().map(RecipeHolder::value).toList();
+        registration.addRecipes(PedestalRecipeCategory.PEDESTAL_RECIPE_TYPE, pedestalRecipes);
+    }
+}
---
+++ src/main/java/net/mreizy/mysticaltrees/compat/PedestalRecipeCategory.java
@@ -0,0 +1,58 @@
+package net.mreizy.mysticaltrees.compat;
+
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.mreizy.mysticaltrees.MysticalTrees;
+import net.mreizy.mysticaltrees.blocks.ModBlocks;
+import net.mreizy.mysticaltrees.recipe.PedestalRecipe;
+import org.jetbrains.annotations.Nullable;
+
+public class PedestalRecipeCategory implements IRecipeCategory<PedestalRecipe> {
+    public static final ResourceLocation UID = ResourceLocation.fromNamespaceAndPath(MysticalTrees.MOD_ID, "infusion");
+    public static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(MysticalTrees.MOD_ID, "textures/jei/infusion.png");
+
+
+    public static final RecipeType<PedestalRecipe> PEDESTAL_RECIPE_TYPE =
+            new RecipeType<>(UID, PedestalRecipe.class);
+
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    public PedestalRecipeCategory(IGuiHelper helper) {
+        this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 86);
+        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.SIDE_PEDESTAL));
+    }
+
+    @Override
+    public RecipeType<PedestalRecipe> getRecipeType() {
+        return PEDESTAL_RECIPE_TYPE;
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.translatable("block.mysticaltrees.side_pedestal");
+    }
+
+    @Override
+    public @Nullable IDrawable getIcon() {
+        return icon;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, PedestalRecipe recipe, IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.INPUT, 54, 34).addIngredients(recipe.getIngredients().get(0));
+
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 104, 34).addItemStack(recipe.getResultItem(null));
+    }
+}
